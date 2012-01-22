73aad54ce5577bddbc8fe0234771d577d31c7167
tedd824 <tedd824@e9a8483c-e51d-b3e1-a26d-f754a9652496>
2012-01-22 03:04:08 +0000

    Add scribe server test class

--- scribe-log4j-appender/src/main/java/com/tedwon/scribe/ScribeJavaServer.java
+++ scribe-log4j-appender/src/main/java/com/tedwon/scribe/ScribeJavaServer.java
@@ -1,9 +1,12 @@
 package com.tedwon.scribe;
 
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.server.THsHaServer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TSimpleServer;
-import org.apache.thrift.transport.TServerSocket;
-import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,7 +20,7 @@ public class ScribeJavaServer {
     public static ScribeLogImpl handler;
 
     public static scribe.thrift.scribe.Processor processor;
-    
+
     public static int PORT = 2463;
 
     public void runServer() {
@@ -27,8 +30,10 @@ public class ScribeJavaServer {
             handler = new ScribeLogImpl();
             processor = new scribe.thrift.scribe.Processor(handler);
 
-            TServerTransport serverTransport = new TServerSocket(PORT);
-            TServer server = new TSimpleServer(processor, serverTransport);
+            TNonblockingServerSocket socket = new TNonblockingServerSocket(PORT);
+
+            final TServer server = new THsHaServer(processor, socket,
+                    new TFramedTransport.Factory(), new TBinaryProtocol.Factory());
 
             System.out.println("Starting the ScribeJavaServer... on PORT: " + PORT);
 
