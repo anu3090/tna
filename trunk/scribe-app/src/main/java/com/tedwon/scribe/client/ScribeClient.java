b1071aeedcd6f5d0a6e12d6efc600c55de026803
tedd824 <tedd824@e9a8483c-e51d-b3e1-a26d-f754a9652496>
2012-01-29 05:11:41 +0000

    Add scribe client class

--- trunk/scribe-app/src/main/java/com/tedwon/scribe/client/ScribeClient.java
+++ trunk/scribe-app/src/main/java/com/tedwon/scribe/client/ScribeClient.java
@@ -1,13 +1,21 @@
 package com.tedwon.scribe.client;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TSocket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scribe.thrift.LogEntry;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -23,6 +31,8 @@ public class ScribeClient {
 
     private static Logger logger = LoggerFactory.getLogger(ScribeClient.class);
 
+    private static String scribeCategory = "default";
+
     public static void main(String[] args) throws Exception {
 
         // Read a file from local file system
@@ -34,11 +44,37 @@ public class ScribeClient {
         BufferedReader reader = new BufferedReader(inputStreamReader);
         String line = reader.readLine();
 
-        while(line != null) {
+
+        // Open connection to Scribe Server
+
+        TSocket sock = new TSocket(new Socket("localhost", 1463));
+        TFramedTransport transport = new TFramedTransport(sock);
+        TBinaryProtocol protocol = new TBinaryProtocol(transport, false, false);
+
+        scribe.thrift.scribe.Client client = new scribe.thrift.scribe.Client(protocol);
+
+        while (line != null) {
 
             System.out.println(line);
 
+            perform(client, line);
+
             line = reader.readLine();
+
         }
+
+        transport.close();
+
+    }
+
+    private static void perform(scribe.thrift.scribe.Client client, String message) throws TException {
+
+        List<LogEntry> logEntries = new ArrayList<LogEntry>(1);
+
+        LogEntry entry = new LogEntry(scribeCategory, message);
+        logEntries.add(entry);
+
+        client.Log(logEntries);
+
     }
 }
