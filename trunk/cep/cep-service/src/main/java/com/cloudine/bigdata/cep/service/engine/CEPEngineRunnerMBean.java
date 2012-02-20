9f8bcda50cdd4ee651ba9ec6d41641b2557c45bc
tedd824@gmail.com <tedd824@gmail.com@e9a8483c-e51d-b3e1-a26d-f754a9652496>
2012-02-20 08:55:23 +0000

    Add basic engine configuration codes as a cep service

--- trunk/cep/cep-service/src/main/java/com/cloudine/bigdata/cep/service/engine/CEPEngineRunnerMBean.java
+++ trunk/cep/cep-service/src/main/java/com/cloudine/bigdata/cep/service/engine/CEPEngineRunnerMBean.java
@@ -1,11 +1,11 @@
 package com.cloudine.bigdata.cep.service.engine;
 
 /**
- * Created by IntelliJ IDEA.
- * User: ted
- * Date: 2/20/12
- * Time: 1:24 오후
- * To change this template use File | Settings | File Templates.
+ * CEP Engine Runner Interface.
+ * <p/>See http://esper.codehaus.org/tutorials/tutorial/quickstart.html
+ *
+ * @author <a href=mailto:iamtedwon@gmail.com">Ted Won</a>
+ * @version 1.0
  */
 public interface CEPEngineRunnerMBean {
 
